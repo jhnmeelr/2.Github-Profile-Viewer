55db54369407d56671b1a1f04a7a2f45ffcfa4dc
jhnmeelr <jhn.meelr@gmail.com>
2016-07-18 21:20:29 +0300

    RepoList Repo Components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -19,7 +19,20 @@ class App extends Component {
             cache: 'false',
             success: function(data){
                 this.setState({userData: data});
-                console.log(data);
+            }.bind(this),
+            error: function(xhr, status, err){
+                this.setState({username: null});
+                alert(err);
+            }.bind(this)
+        });
+    }
+    getUserRepos(){
+        $.ajax({
+            url: 'https://api.github.com/users/'+this.state.username+'/repos?per_page='+this.state.perPage+'&client_id='+this.props.clientId+'&client_secret='+this.props.clientSecret+'&sort=created',
+            dataType: 'json',
+            cache: 'false',
+            success: function(data){
+                this.setState({userRepos: data});
             }.bind(this),
             error: function(xhr, status, err){
                 this.setState({username: null});
@@ -29,11 +42,12 @@ class App extends Component {
     }
     componentDidMount(){
         this.getUserData();
+        this.getUserRepos();
     }
     render(){
         return (
             <div>
-                <Profile userData={this.state.userData} />
+                <Profile {...this.state} />
             </div>
         );
     }
