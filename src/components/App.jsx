657717312c0c642a10b0326e766e218e27deeae0
jhnmeelr <jhn.meelr@gmail.com>
2016-07-18 20:00:35 +0300

    Profile Component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,41 @@
 import React, {Component} from 'react';
 import ReactDOM from "react-dom";
+import Profile from "./github/Profile.jsx";
 
 class App extends Component {
     constructor(props){
         super(props);
         this.state = {
-            username: 'melnykivan',
+            username: 'jhnmeelr',
             userData: [],
             userRepos: [],
             perPage: 5
         }
     }
+    getUserData(){
+        $.ajax({
+            url: 'https://api.github.com/users/'+this.state.username+'?client_id='+this.props.clientId+'&client_secret='+this.props.clientSecret,
+            dataType: 'json',
+            cache: 'false',
+            success: function(data){
+                this.setState({userData: data});
+                console.log(data);
+            }.bind(this),
+            error: function(xhr, status, err){
+                this.setState({username: null});
+                alert(err);
+            }.bind(this)
+        });
+    }
+    componentDidMount(){
+        this.getUserData();
+    }
     render(){
-        return(
+        return (
             <div>
-                {this.state.username}
+                <Profile userData={this.state.userData} />
             </div>
-        )
+        );
     }
 };
 
@@ -29,4 +48,4 @@ App.defaultProps = {
     clientSecret: 'bbe9b0afc06fa2fc4157b403462f2f4c6c709689',
 }
 
-export default App
+export default App;
